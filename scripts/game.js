d2304f95ec44facecf5308279f9ddc75bc383fe6
NoaKo <noako.js@gmail.com>
2021-11-14 22:42:40 +0200

    tic-tac-toe game learned in web course

---
+++ scripts/game.js
@@ -0,0 +1,113 @@
+function resetGameStatus(){
+    gameIsOver=false;
+    activePlayer=0;
+    currentRound=1;
+    gameOverElement.firstElementChild.innerHTML=
+    "You won, <span id='winner-name'>PLAYER NAME</span>!";
+    gameOverElement.style.display="none";
+
+    let gameBoardIndex=0;
+    for(let i=0; i<3; i++)
+    {
+        for(let j=0; j<3; j++)
+        {
+            gameData[i][j]=0;
+            gameFieldElements[gameBoardIndex].textContent="";
+            gameFieldElements[gameBoardIndex].classList.remove("disabled");
+            gameBoardIndex++;
+        }
+    }
+}
+
+function startNewGame(){
+    if(players[0].name==="" || players[1].name==="")
+    {
+        alert("please set player names for both players!");
+        return;
+    }
+    resetGameStatus();
+
+    activePlayerNameElement.textContent=players[activePlayer].name;
+    gameAreaElement.style.display="block";
+}
+
+function switchPlayer(){
+    if(activePlayer===0)
+    {
+        activePlayer=1;
+    }   
+    else
+    {
+        activePlayer=0;
+    } 
+    activePlayerNameElement.textContent=players[activePlayer].name;      
+}
+
+function selectGameField(event){
+    if(event.target.tagName!=="LI" || gameIsOver){
+        return;
+    }   
+    const selectedField=event.target;
+    const selectedColumn=selectedField.dataset.col-1;
+    const selectedRow=selectedField.dataset.row-1;
+
+    if(gameData[selectedRow][selectedColumn]>0)
+    {
+        alert("please select an empty field!");
+        return;
+    }
+    selectedField.textContent=players[activePlayer].symbol;
+    selectedField.classList.add("disabled");
+
+    
+
+    gameData[selectedRow][selectedColumn]=activePlayer+1;
+
+    const winnerId=checkForGameOver();
+    if(winnerId!==0)
+    {
+        endGame(winnerId);
+    }
+    currentRound++;
+    switchPlayer();
+}
+
+function checkForGameOver(){
+    for(var i=0; i<3; i++)
+    {
+        //rows
+        if(gameData[i][0]>0 && gameData[i][0]==gameData[i][1] && gameData[i][1]==gameData[i][2]){
+            return gameData[i][0];
+        }
+        //columns
+        if(gameData[0][i]>0 && gameData[0][i]==gameData[1][i] && gameData[1][i]==gameData[2][i]){
+            return gameData[0][i];
+        }
+    }
+    //main diagonal
+    if(gameData[0][0]>0 && gameData[0][0]==gameData[1][1] && gameData[1][1]==gameData[2][2]){
+        return gameData[0][0];
+    }
+    //secondary diagonal
+    if(gameData[0][2]>0 && gameData[0][2]==gameData[1][1] && gameData[1][1]==gameData[2][0]){
+        return gameData[0][2];
+    }
+    if(currentRound==9)
+    {
+        return -1; //a value for a draw
+    }
+    return 0; //game is not over yet
+}
+
+function endGame(winnerId){
+    gameIsOver=true;
+    gameOverElement.style.display="block";
+    if(winnerId>0)
+    {
+        gameOverElement.firstElementChild.firstElementChild.textContent=players[winnerId-1].name;
+    }
+    else{
+        gameOverElement.firstElementChild.textContent="It\'s a draw!";
+    }
+    
+}
